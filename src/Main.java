3a7a32a1a874bcce1b28e155f5c4af1d00d709ed
Lee Hyeon Jeong <hyeont0r1@gmail.com>
2018-05-25 18:19:18 +0900

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] asdf = new int[N];
+        for (int i=0; i<N;i++)
+            asdf[i] = sc.nextInt();
+
+        for (int j=0; j<N;j++)
+            System.out.println(asdf[j]);
+    }
+}
